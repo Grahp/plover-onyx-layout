42dd225b3b23fd70ed8967daaaca47b5dcdf4b0c
Grahp <grahp@grahp.org>
2025-05-09 17:53:28 -0400

    Switch all ' to " >:(

--- onyx_layout.py
+++ onyx_layout.py
@@ -1,17 +1,17 @@
 from plover.system.english_stenotype import *
 
 KEYS = (
-  "A-", '#-',
-  "+-", 'K-', 'S-', 'P-', 'T-', 'R-', 'L-',
-  '^-',
-  'J', 'E', 'I', 'U',
-  '-V',
-  '-R', '-L', '-K', '-P', '-T', '-S',
-  '-D', '-Z', '-Y', '-W'
+  "A-", "#-",
+  "+-", "K-", "S-", "P-", "T-", "R-", "L-",
+  "^-",
+  "J", "E", "I", "U",
+  "-V",
+  "-R", "-L", "-K", "-P", "-T", "-S",
+  "-D", "-Z", "-Y", "-W"
 )
 
 IMPLICIT_HYPHEN_KEYS = ("J", "E", "I", "U")
-SUFFIX_KEYS = ("-V", "-D", "-Z", "-Y", "-W")
+SUFFIX_KEYS = ("-V", "-D", "-Z", "-Y", "-W", "-DZ")
 NUMBERS = {}
 NUMBER_KEY = None
 FERAL_NUMBER_KEY = False
