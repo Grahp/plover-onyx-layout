32b29bb1bedc64828d4bb8073e680a8ecfd7446c
Grahp <grahp@grahp.org>
2025-07-01 17:40:59 -0400

    Update to new version

--- onyx_layout.py
+++ onyx_layout.py
@@ -1,20 +1,18 @@
 from plover.system.english_stenotype import *
 
 KEYS = (
-  "A-", "#-",
-  "+-", "K-", "S-", "P-", "T-", "R-", "L-",
-  "^-",
-  "J", "E", "I", "U",
-  "-V",
-  "-R", "-L", "-K", "-P", "-T", "-S",
-  "-D", "-Z", "-Y", "-W"
+  "A-", "+-",
+  "*-", "T-", "S-", "P-", "K-", "L-", "R-",
+  "Y", "E", "I", "U", "^",
+  "-L", "-R", "-P", "-K", "-T", "-S", "*",
+  "-r", "-d", "-s"
 )
 
-IMPLICIT_HYPHEN_KEYS = ("J", "E", "I", "U")
-SUFFIX_KEYS = ("-V", "-D", "-Z", "-Y", "-W", "-DZ", "-DZYW", "-ZW")
+IMPLICIT_HYPHEN_KEYS = ("Y", "E", "I", "U", "^")
+SUFFIX_KEYS = ("+", "A", "A+", "-r", "-d", "-s", "-rs", "-rd", "-ds", "-rds", "+-r", "+-d", "+-s", "+-rs", "+-rd", "+-ds", "+-rds", "A-r", "A-d", "A-s", "A-rs", "A-rd", "A-ds", "A-rds", "A+-r", "A+-d", "A+-s", "A+-rs", "A+-rd", "A+-ds", "A+-rds")
 NUMBERS = {}
 NUMBER_KEY = None
 FERAL_NUMBER_KEY = False
-UNDO_STROKE_STENO = "-V"
+UNDO_STROKE_STENO = "-R"
 
 ## Add Default keymaps
